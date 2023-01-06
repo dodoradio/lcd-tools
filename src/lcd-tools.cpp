267ef8f54ce608ab6c78ba59cd0db90e3a9c156f
Arseniy Movshev <dodoradio@outlook.com>
2023-01-06 01:38:19 +0000

    Read machine from config file Removes the command line switch and reads the machine codename from machine.conf config file

--- src/lcd-tools.cpp
+++ src/lcd-tools.cpp
@@ -3,40 +3,48 @@
 
 #include <CLI/CLI.hpp>
 
+#include <QSettings>
+
+const char* CONFIG_FILE = "/etc/asteroid/machine.conf";
+
 using namespace AsteroidOS::LCD_Tools;
 
 int main( int argc, char** argv ) {
-	CLI::App app{"lcd-tools: Various controls for watches with a secondary displays"};
 
-	bool machineCatfish = false;
-	bool machineKoi = false;
+	QSettings m_settings(CONFIG_FILE, QSettings::IniFormat);
+	const QString machineCodename = m_settings.value("Identity/MACHINE", "unknown").toString();
+
+	CLI::App app{"lcd-tools: Various controls for watches with a secondary displays"};
 
 	bool syncTime = false;
 
 	int koiDisplayColor = -1;
-	bool koiTimepiece = 0;
+	bool koiTimepiece = false;
 
-	app.add_flag("--catfish",machineCatfish,"Ticwatch Pro mode");
-	app.add_flag("--koi",machineKoi,"Casio WSD-F10/20 mode");
 	app.add_flag("--sync-time",syncTime,"Sync lcd time with linux time");
-	app.add_option("--set-display",koiDisplayColor,"(Koi) set display color");
-	app.add_flag("--prepare-timepiece",koiTimepiece,"(Koi) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
+
+	if (machineCodename == "koi") {
+		app.add_option("--set-display",koiDisplayColor,"(Koi) set display color");
+		app.add_flag("--prepare-timepiece",koiTimepiece,"(Koi) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
+	}
 
 	try{
 		app.parse(argc,argv);
-		if (machineCatfish) {
-			if (syncTime) {
-				CatfishSyncTime();
-			}
-		} else if (machineKoi) {
-			if (syncTime) {
-				KoiSyncTime();
-			}
-			if (koiDisplayColor != -1) {
-				KoiSetDisplayColor(koiDisplayColor);
-			}
-			if (koiTimepiece) {
-				KoiPrepareTimepiece();
+		if (argc >= 2) {
+			if (machineCodename == "catfish") {
+				if (syncTime) {
+					CatfishSyncTime();
+				}
+			} else if (machineCodename == "koi") {
+				if (syncTime) {
+					KoiSyncTime();
+				}
+				if (koiDisplayColor != -1) {
+					KoiSetDisplayColor(koiDisplayColor);
+				}
+				if (koiTimepiece) {
+					KoiPrepareTimepiece();
+				}
 			}
 		} else
 			throw CLI::CallForAllHelp();
