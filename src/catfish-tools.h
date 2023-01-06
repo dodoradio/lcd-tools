41708f627293a0c13cfa449d0e980f2bdffde7be
Ed Beroset <beroset@ieee.org>
2023-01-06 17:13:12 -0500

    Alter function signatures for callback
    This alters the function signatures to add the codename of each watch as
    another level of namespace and makes the functions void(int) to be
    compatible directly with CLI11's callbacks.
    
    Change function signature for use as callback
    
    This matches the change to the header file in changing the function to a
    void(int).
    
    Restructure using SpiMsg type
    
    This restructures the code to use a SpiMsg type (alias of
    std::array<uint8_t, 7>) and modifies to simplify use of functions as
    callbacks.  Functions not part of the interface are static.
    
    Use more features of CLI11
    
    This relies more heavily on the callback feature of CLI11 and simplifies
    and clarifies the interface.
    
    Restore missing return
    
    If the file does not open, the function should return after reporting
    this fact rather than trying to write to the file.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/catfish-tools.h
+++ src/catfish-tools.h
@@ -1,7 +1,7 @@
 #ifndef ASTEROIDOS_CATFISH_TOOLS_H
 #define ASTEROIDOS_CATFISH_TOOLS_H
 
-namespace AsteroidOS::LCD_Tools {
-	int CatfishSyncTime();
+namespace AsteroidOS::LCD_Tools::Catfish {
+	void SyncTime(int);
 }
 #endif //ASTEROIDOS_CATFISH_TOOLS_H
--- src/koi-tools.cpp
+++ src/koi-tools.cpp
@@ -1,5 +1,6 @@
 #include "koi-tools.h"
 
+#include <array>
 #include <iostream>
 #include <dlfcn.h>
 #include <MGConfItem>
@@ -8,42 +9,45 @@
 #include <QDate>
 #include <QFile>
 
-int AsteroidOS::LCD_Tools::KoiSyncTime() {
-	char outputBuff[7] = {0xFE,0x01,0x01,0x00,0x00,0x00,0x00};
-	outputBuff[3] = (uint8_t) !MGConfItem("/org/asteroidos/settings/use-12h-format").value().toBool();
-	Write(outputBuff);
+using SpiMsg = std::array<uint8_t, 7>;
 
-	QDate dateNow = QDate::currentDate();
-	QTime timeNow = QTime::currentTime();
-	outputBuff[0] = (uint8_t) 0x50;
-	outputBuff[1] = (uint8_t) 0;
-	outputBuff[2] = (uint8_t)((((dateNow.year() - 2000) & 0xEF) << 1) | ((dateNow.month() & 0x08) >> 3));
-	outputBuff[3] = (uint8_t) ((dateNow.month() & 0x07) << 5) | ((dateNow.day() & 0x1F));
-	outputBuff[4] = (uint8_t) (((dateNow.dayOfWeek() & 0x07) << 5) |  (timeNow.hour() & 0x1F));
-	outputBuff[5] = (uint8_t) (timeNow.minute() & 0xFF);
-	outputBuff[6] = (uint8_t) ((timeNow.second() + 1) & 0xFF); //the original casio code seems to add one second here to compensate for an update delay in the subcpu
-	return Write(outputBuff);
-}
-
-int AsteroidOS::LCD_Tools::KoiSetDisplayColor(bool value) {
-	char outputBuff[7] = {0xFE,0x01,0x05,0x00,0x00,0x00,0x00};
-	outputBuff[3] = (uint8_t) value;
-	return Write(outputBuff);
-}
-
-int AsteroidOS::LCD_Tools::KoiPrepareTimepiece() {
-	char outputBuff[7] = {0x02,0xC1,0xBE,0x78,0x3F,0x91,0xC7};
-	return Write(outputBuff);
-}
-
-int AsteroidOS::LCD_Tools::Write(char* data) {
+static void Write(const SpiMsg& data) {
 	QFile multiSensorFile("/dev/MultiSensors_CD_01");
 	if(!multiSensorFile.open(QIODevice::WriteOnly | QIODevice::Text))
 	{
 		qCritical("Unable to open file for write. Check permissions");
-		return(-1);
-    }
-	multiSensorFile.write(data,7);
+		return;
+	}
+	multiSensorFile.write((const char *)data.data(), data.size());
 	multiSensorFile.close();
-	return 1;
 }
+
+static void KoiSet12H(bool value) {
+	Write({0xFE,0x01,0x01,value,0x00,0x00,0x00});
+}
+
+namespace AsteroidOS::LCD_Tools::Koi {
+
+void SyncTime(int) {
+	KoiSet12H(!MGConfItem("/org/asteroidos/settings/use-12h-format").value().toBool());
+	QDate dateNow = QDate::currentDate();
+	QTime timeNow = QTime::currentTime();
+	Write({
+	(uint8_t) 0x50,
+	(uint8_t) 0,
+	(uint8_t) ((((dateNow.year() - 2000) & 0xEF) << 1) | ((dateNow.month() & 0x08) >> 3)),
+	(uint8_t) ((dateNow.month() << 5) | (dateNow.day() & 0x1F)),
+	(uint8_t) ((dateNow.dayOfWeek() << 5) | (timeNow.hour() & 0x1F)),
+	(uint8_t) (timeNow.minute() & 0xFF),
+	(uint8_t) ((timeNow.second() + 1) & 0xFF) //the original casio code seems to add one second here to compensate for an update delay in the subcpu
+	});
+}
+
+void SetDisplayColor(bool value) {
+	Write({0xFE,0x01,0x05,value,0x00,0x00,0x00});
+}
+
+void PrepareTimepiece(int) {
+	Write({0x02,0xC1,0xBE,0x78,0x3F,0x91,0xC7});
+}
+} // end of namespace AsteroidOS::LCD_Tools::Koi
