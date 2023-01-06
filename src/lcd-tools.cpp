d8c47f3266793301b091777e8a6e6624ac9c18a0
Ed Beroset <beroset@ieee.org>
2023-01-06 17:18:33 -0500

    Use more features of CLI11
    This relies more heavily on the callback feature of CLI11 and simplifies
    and clarifies the interface.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/lcd-tools.cpp
+++ src/lcd-tools.cpp
@@ -7,7 +7,6 @@
 
 const char* CONFIG_FILE = "/etc/asteroid/machine.conf";
 
-using namespace AsteroidOS::LCD_Tools;
 
 int main( int argc, char** argv ) {
 
@@ -15,41 +14,18 @@ int main( int argc, char** argv ) {
 	const QString machineCodename = m_settings.value("Identity/MACHINE", "unknown").toString();
 
 	CLI::App app{"lcd-tools: Various controls for watches with a secondary displays"};
-
-	bool syncTime = false;
-
-	int koiDisplayColor = -1;
-	bool koiTimepiece = false;
-
-	app.add_flag("--sync-time",syncTime,"Sync lcd time with linux time");
-
 	if (machineCodename == "koi") {
-		app.add_option("--set-display",koiDisplayColor,"(Koi) set display color");
-		app.add_flag("--prepare-timepiece",koiTimepiece,"(Koi) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
+		using namespace AsteroidOS::LCD_Tools::Koi;
+		app.add_flag("--sync-time",SyncTime,"Sync lcd time with linux time");
+		app.add_flag("--white-background",[](int){SetDisplayColor(true);},"(Koi) set display background to white");
+		app.add_flag("--black-background",[](int){SetDisplayColor(false);},"(Koi) set display background to black");
+		app.add_flag("--prepare-timepiece",PrepareTimepiece,"(Koi) prepare watch for power off into timekeeping mode. You will then need to shut it down manually");
+	} 
+	else if (machineCodename == "catfish") {
+		using namespace AsteroidOS::LCD_Tools::Catfish;
+		app.add_flag("--sync-time",SyncTime,"Sync lcd time with linux time");
 	}
 
-	try{
-		app.parse(argc,argv);
-		if (argc >= 2) {
-			if (machineCodename == "catfish") {
-				if (syncTime) {
-					CatfishSyncTime();
-				}
-			} else if (machineCodename == "koi") {
-				if (syncTime) {
-					KoiSyncTime();
-				}
-				if (koiDisplayColor != -1) {
-					KoiSetDisplayColor(koiDisplayColor);
-				}
-				if (koiTimepiece) {
-					KoiPrepareTimepiece();
-				}
-			}
-		} else
-			throw CLI::CallForAllHelp();
-	} catch (const CLI::ParseError &e){
-		return app.exit(e);
-	}
+	CLI11_PARSE(app, argc, argv);
 	return 0;
 }
