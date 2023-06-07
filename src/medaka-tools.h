de10775e1bc24c7f315d89125f84a658db2c1f50
Arseniy Movshev <dodoradio@outlook.com>
2023-06-07 12:58:09 +0100

    Add support for medaka. This adds LCD code for medaka, the WSD-F21HR, successor to koi/ayu. The time sync code is identical to koi. The display colour code uses a different set of bits to encode black vs. white. The 'timepiece mode' command is still 'coming soon'. It is quite different, as it needs to select whether it wants to show the date/barometer and battery/step/altimeter data on the top and bottom displays.
    Signed-off-by: Arseniy Movshev <dodoradio@outlook.com>

---
+++ src/medaka-tools.h
@@ -0,0 +1,8 @@
+#ifndef ASTEROIDOS_MEDAKA_TOOLS_H
+#define ASTEROIDOS_MEDAKA_TOOLS_H
+
+namespace AsteroidOS::LCD_Tools::Medaka {
+	void SyncTime(int);
+	void SetDisplayColor(bool value);
+}
+#endif //ASTEROIDOS_MEDAKA_TOOLS_H
